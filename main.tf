4f981d34bf0ec7e3fd2ca9e091e195eb4d5124a5
challenger <philip.gumm@outlook.com>
2025-01-25 14:19:24 +0800

    forget the for_each for the disk attachment

--- main.tf
+++ main.tf
@@ -192,6 +192,8 @@ resource "azurerm_managed_disk" "windows_data_disks" {
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "windows-attach-disk" {
+  for_each = azurerm_managed_disk.windows_data_disks
+  
   managed_disk_id    = azurerm_managed_disk.windows_data_disks[each.key].id
   virtual_machine_id = azurerm_windows_virtual_machine[each.key].id
   lun                = 0
