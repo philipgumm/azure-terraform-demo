2db0a2ee24ae0bda9fbd0f31a6fe5645d39782f3
Gummuk <philip.gumm@outlook.com>
2025-02-11 15:53:39 +0800

    removed nsg

--- main.tf
+++ main.tf
@@ -61,27 +61,6 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  tags = {
-    environment = "Production"
-  }
-}
 
 resource "azurerm_network_interface" "linux-nic" {
   for_each = var.linux_vm_configurations
