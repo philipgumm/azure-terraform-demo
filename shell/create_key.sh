0800f9faa8d09e5c5b0b3100ca3c92c6d99b7eb3
Gummuk <philip.gumm@outlook.com>
2025-02-12 17:09:18 +0800

    added logging check

--- shell/create_key.sh
+++ shell/create_key.sh
@@ -1,6 +1,2 @@
 # Server keys for Ansible
 ssh-keygen -t rsa -b 4096 -C "azure-terraform-demo_ssh-key" -f ~/.ssh/azure-terraform-demo_ssh-key
-
-
-
-ssh-keygen -m PEM -t rsa -b 4096 -f ~/.ssh/id_rsa.pem
