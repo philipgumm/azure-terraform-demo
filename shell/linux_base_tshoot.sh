b28df0dd929e8bc4c6ec182b14d46c39a4c8848f
Gummuk <philip.gumm@outlook.com>
2025-02-13 09:02:02 +0800

    t-shoot steps for linux script download

---
+++ shell/linux_base_tshoot.sh
@@ -0,0 +1,13 @@
+ls -l /var/lib/waagent/custom-script/download/
+sudo cd -l /var/lib/waagent/custom-script/download/
+
+curl -O "https://labmanagementstorage01.blob.core.windows.net/public-azure-terraform-demo/linux_base.sh"
+sh linux_base.sh
+
+cat /var/log/azure/custom-script/handler.log | tail -50
+
+cat /var/lib/waagent/custom-script/download/0/stdout
+cat /var/lib/waagent/custom-script/download/0/stderr
+
+
+
