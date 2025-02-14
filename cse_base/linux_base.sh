efb4467b7ae672eb5ab172bd325d45bbde145cba
Gummuk <philip.gumm@outlook.com>
2025-02-14 11:03:14 +0800

    allowed ansible installation

--- cse_base/linux_base.sh
+++ cse_base/linux_base.sh
@@ -12,7 +12,7 @@ fi
 
 #timeout 600 sudo dnf install -y epel-release >> /tmp/custom_script.log 2>&1
 #timeout 1200 sudo dnf update -y >> /tmp/custom_script.log 2>&1
-#timeout 600 sudo dnf install -y ansible >> /tmp/custom_script.log 2>&1
+timeout 100 sudo dnf install -y ansible >> /tmp/custom_script.log 2>&1
 
 
 ansible --version >> /tmp/custom_script.log 2>&1 
